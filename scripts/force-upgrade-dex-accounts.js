2e41d40e6f8116c99e2c7c69475eb9960ad14367
aleksandr-hramcov <aleksandr.hramcov@gmail.com>
2021-08-10 19:30:51 +0300

    force upgrade accounts script fixes

--- scripts/force-upgrade-dex-accounts.js
+++ scripts/force-upgrade-dex-accounts.js
@@ -5,10 +5,14 @@ let dexOwner;
 
 const DEX_ROOT_ADDRESS = '0:943bad2e74894aa28ae8ddbe673be09a0f3818fd170d12b4ea8ef1ea8051e940';
 const DEX_OWNER_ADDRESS = '';
-const DEX_OWNER_KEYS = {};
+const DEX_OWNER_WALLET_TYPE = 'SafeMultisigWallet';
+const DEX_OWNER_KEYS =  {
+    public: '',
+    secret: ''
+};
 
 const afterRun = async (tx) => {
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    await new Promise(resolve => setTimeout(resolve, 1000));
 };
 
 const data = fs.readFileSync('./dex_accounts.json', 'utf8');
@@ -18,7 +22,13 @@ async function main() {
   const dexRoot = await locklift.factory.getContract('DexRoot');
   dexRoot.setAddress(DEX_ROOT_ADDRESS);
 
-  const dexOwner = await locklift.factory.getAccount('SafeMultisigWallet', 'safemultisig');
+
+  let dexOwner;
+  if (DEX_OWNER_WALLET_TYPE === 'SafeMultisigWallet'){
+      dexOwner = await locklift.factory.getAccount('SafeMultisigWallet', 'safemultisig');
+  } else {
+      dexOwner = await locklift.factory.getAccount(DEX_OWNER_WALLET_TYPE);
+  }
   dexOwner.setAddress(DEX_OWNER_ADDRESS);
   dexOwner.afterRun = afterRun;
 
@@ -27,7 +37,7 @@ async function main() {
 
   for(let indx in dexAccounts) {
       const accountData = dexAccounts[indx];
-      console.log(`${indx + 1}/${dexAccounts.length}: Upgrading DexAccount(${accountData.dexAccount}). owner = ${accountData.owner}`);
+      console.log(`${1 + (+indx)}/${dexAccounts.length}: Upgrading DexAccount(${accountData.dexAccount}). owner = ${accountData.owner}`);
       const tx = await dexOwner.runTarget({
           contract: dexRoot,
           method: 'forceUpgradeAccount',
@@ -36,7 +46,7 @@ async function main() {
               send_gas_to: dexOwner.address
           },
           value: locklift.utils.convertCrystal(6, 'nano'),
-          DEX_OWNER_KEYS
+          keyPair: DEX_OWNER_KEYS
       });
       console.log(`Transaction id: ${tx.transaction.id}`);
       console.log(``);
